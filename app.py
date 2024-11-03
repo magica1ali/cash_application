9e66d9b81c9a5fe13e0ba33b13724f9ad49a51e6
magica1ali <120230269+magica1ali@users.noreply.github.com>
2024-11-03 15:03:05 -0800

    Checking Dataload

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ detail['poMatch']  = detail['Purchase Order Number']
 remit_review = remit[['poMatch','Amount Paid($)','Store Number','Invoice Date']]
 detail_review = detail[['poMatch','Ship-To Customer Name','Invoice Number','Requested Delivery Date']]
 
-#dataload = pd.merge(remit_review,detail_review, on='poMatch', how ='left')
-
-st.write(remit['poMatch'].dtypes )
-st.write(detail['poMatch'].dtypes)
+dataload = pd.merge(remit_review,detail_review, on='poMatch', how ='left')
+st.write(dataload)
+#st.write(remit['poMatch'].dtypes )
+#st.write(detail['poMatch'].dtypes)
