da942acec1b1099b54a25e284a9fc0695e6d7cf4
magica1ali <120230269+magica1ali@users.noreply.github.com>
2024-11-03 14:18:41 -0800

    Setting up merge

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.title("Streamlit App for Walmart Cash Application")
 # Ingest Data 
 
 #### Create a file uploader for remittance file in your Streamlit app ####
-remittance_files = st.file_uploader("Select Walmart Remittance Detail. Choose a CSV file", type='csv', accept_multiple_files=False)
+remittance_file = st.file_uploader("Select Walmart Remittance Detail. Choose a CSV file", type='csv', accept_multiple_files=False)
 
 #### Create a file uploader for DG detail file in your Streamlit app ####
 detail_file = st.file_uploader("Select Corresponding Details Report. Choose a CSV File", type ='csv', accept_multiple_files=False)
@@ -18,5 +18,17 @@ detail_file = st.file_uploader("Select Corresponding Details Report. Choose a CS
 #### Display remittance detail file
 
 st.title("Remittance Detail")
-remit = pd.read_csv(remittance_files)
+remit = pd.read_csv(remittance_file)
+st.write(remit)
+
+#### Display Details Report 
+st.title("Details Report")
+detail = pd.read_csv(detail_file)
+st.write(detail)
+
+# Combine into New Detail Frame
+
+#### Merge()  VLOOKUP Equivalent
+
+remit['poMatch']  = remit['Invoice Number']
 st.write(remit)
