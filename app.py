9f2aa5496336a1186b1e153318e5092a06b85685
magica1ali <120230269+magica1ali@users.noreply.github.com>
2024-11-03 19:30:16 -0800

    updated code

--- app.py
+++ app.py
@@ -8,39 +8,42 @@ import streamlit as st
 
 st.title("Streamlit App for Walmart Cash Application")
 
-# Ingest Data 
+## Ingest Data 
 
-#### Create a file uploader for remittance file in your Streamlit app ####
+### Create a file uploader for remittance file in your Streamlit app ####
 remittance_file = st.file_uploader("Select Walmart Remittance Detail. Choose a CSV file", type='csv', accept_multiple_files=False)
 
-#### Create a file uploader for DG detail file in your Streamlit app ####
+### Create a file uploader for DG detail file in your Streamlit app ####
 detail_file = st.file_uploader("Select Corresponding Details Report. Choose a CSV File", type ='csv', accept_multiple_files=False)
 
-#### Store remittance detail file
+### Store remittance detail file
 
 remit = pd.read_csv(remittance_file)
 
-#### Store Details Report 
+### Store Details Report 
 
 detail = pd.read_csv(detail_file)
 
-# Combine into New Detail Frame
+## Merge()  VLOOKUP Equivalent
 
-#### Merge()  VLOOKUP Equivalent
-
+### Change data type to string
 remit['poMatch']  = remit['Invoice Number'].astype('string')
 detail['poMatch']  = detail['Purchase Order Number'].astype('string')
+
+### Clean up string to remove '.0'
 remit['poMatch'] = remit['poMatch'].str.replace(r'\.0$', '', regex=True)
+
+### Save new data frames to be merged 
 remit_review = remit[['poMatch','Amount Paid($)']]
 detail_review = detail[['Invoice Number','poMatch']]
 
-st.title("Merged Datae-frame")
+
 dataload = remit_review.merge(detail_review, on='poMatch', how ='left')
-#### Remove duplicates 
+
+### Remove duplicates 
 dataload_cleaned = dataload.drop_duplicates()
 
-#### Check Shape of data 
-st.write(dataload.shape)
-st.write(dataload_cleaned.shape)
-st.write(remit_review.shape)
-st.write(detail_review.shape)
+
+## Down Data 
+st.title("Merged Datae-frame")
+st.write(dataload_cleaned)
