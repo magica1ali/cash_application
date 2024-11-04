d5c3e7e5835f076d93993652b7ecdae8e8160cd3
magica1ali <120230269+magica1ali@users.noreply.github.com>
2024-11-03 18:22:27 -0800

    remit_review

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ detail_review = detail[['poMatch','Invoice Number']]
 dataload = remit_review.merge(detail_review, on='poMatch', how ='left')
 #st.write(dataload)
 #st.write(remit_review['poMatch'].dtypes )
-st.write(detail_review[['poMatch','Invoice Number']])
+st.write(remit_review['poMatch'])
