f1a2d4598943a6d0d484846021b47d2ecfcb509e
magica1ali <120230269+magica1ali@users.noreply.github.com>
2024-11-03 18:30:03 -0800

    changed to float

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ detail = pd.read_csv(detail_file)
 
 #### Merge()  VLOOKUP Equivalent
 
-remit['poMatch']  = remit['Invoice Number'].astype('string')
-detail['poMatch']  = detail['Purchase Order Number'].astype('string')
+remit['poMatch']  = remit['Invoice Number'].astype('float')
+detail['poMatch']  = detail['Purchase Order Number'].astype('float')
 remit_review = remit[['poMatch','Amount Paid($)']]
 detail_review = detail[['Invoice Number','poMatch']]
 
